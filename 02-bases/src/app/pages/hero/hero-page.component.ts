cbe6a8e63e2b47fec1dddede43c24ccf9174958d
Jean Harold Varas Ramirez <j.varas@ucss.edu.pe>
2026-06-18 16:11:10 -0500

    02 Bases Angular

---
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,40 @@
+import { UpperCasePipe } from '@angular/common';
+import { Component, computed, OnInit, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './hero-page.component.html',
+  imports : [UpperCasePipe]
+})
+
+export class HeroPageComponent  {
+
+  name = signal ('Ironman');
+  age = signal ('45');
+
+  heroDescrption = computed(()=>{
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  })
+
+  capitalizeName = computed(()=>this.name().toUpperCase());
+
+  getHeroDescription() {
+    return `${this.name()} - ${this.age()}`;
+  }
+
+  changeHero() {
+      this.name.set('Spiderman');
+      this.age.set('22');
+  }
+
+  changeAge() {
+      this.age.set('60');
+  }
+
+  resetForm() {
+      this.name.set('Ironman');
+      this.age.set('45');
+  }
+
+
+}
