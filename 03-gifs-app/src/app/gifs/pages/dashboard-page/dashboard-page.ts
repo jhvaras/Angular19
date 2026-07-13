b2d47ae36a92d40af3f58db4d16bd9d7f2f1e427
Jean Harold Varas Ramirez <j.varas@ucss.edu.pe>
2026-07-13 18:04:06 -0500

    GifAPP - Pensemos en componentes

---
+++ 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenuHeader } from "../../components/side-menu-header/side-menu-header";
+import { SideMenuOptions } from "../../components/side-menu-options/side-menu-options";
+import { SideMenu } from "../../components/side-menu/side-menu";
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [
+    RouterOutlet,
+    SideMenu
+  ],
+  templateUrl: './dashboard-page.html',
+})
+export default class DashboardPage { }
---
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production : true,
+  companyName : 'GifsApp',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+    // Apu Key
+
+    // URL
+
+};
