2472cad347b7b53c5737eae94a5d84cc42a9b3d2
alucard <thewiner57@gmail.com>
2026-07-23 20:31:06 -0500

    Add Session 11 countrys intermedio de dev/talles

--- angular-country-app/src/app/app.component.ts
+++ angular-country-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- angular-country-app/src/app/country/components/top-menu/top-menu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-
-@Component({
-  selector: 'country-top-menu',
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './top-menu.component.html',
-})
-export class TopMenuComponent {}
